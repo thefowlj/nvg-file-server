a2ba55d77fbf9358262aa3d6e2e6766aa272956b
Jon Fowler <jfowler27@gmail.com>
2021-01-16 10:51:45 -0700

    Add temporary file upload functionality

--- index.js
+++ index.js
@@ -18,14 +18,21 @@ const FILE_SIZE_CONSTANT = 1024 * 1024;
 const MAX_FILE_SIZE_MB = 4096;
 const MAX_FILE_SIZE = FILE_SIZE_CONSTANT * MAX_FILE_SIZE_MB;
 const FILE_DIR = process.env.FILE_DIR;
+const TEMP_DIR = process.env.TEMP_DIR;
 
-// Create storage directory if it does not exist on startup
-try {
-  if (!fs.existsSync(FILE_DIR)) {
-    fs.mkdirSync(FILE_DIR);
+// Create storage directorIES if it does not exist on startup
+checkStorage(FILE_DIR);
+checkStorage(TEMP_DIR);
+
+// Create directories at specific path
+function checkStorage(dir) {
+  try {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+  } catch (err) {
+    console.log(err);
   }
-} catch (err) {
-  console.log(err);
 }
 
 // GET landing page
@@ -50,23 +57,79 @@ server.get('/api/file/:filename', (req, res) => {
 // GET request to delete specific file
 server.get('/api/file/delete/:filename', (req, res) => {
   fs.unlink(FILE_DIR + req.params.filename, (err) => {
-    if(err) {
+    if (err) {
       console.log(err);
     }
     res.redirect('/');
-  })
+  });
 });
 
 // POST file for upload
 server.post('/api/upload', (req, res) => {
-  const form = formidable({ maxFileSize: MAX_FILE_SIZE });
-  form.parse(req, (err, fields, files) => {
-    fs.rename(files.filetoupload.path, FILE_DIR + files.filetoupload.name, (err) => {
-      if (err) throw err;
-      res.redirect('/');
-    });
+  addFile(req, res, FILE_DIR, '/');
+});
+
+// GET temp upload html page
+server.get('/temp', (req, res) => {
+  res.sendFile(path.join(__dirname, 'temp.html'));
+});
+
+// GET shortcut route redirect to /temp
+server.get('/t', (req, res) => {
+  res.redirect('/temp');
+});
+
+// GET shortcut route to download temp file
+server.get('/td', (req, res) => {
+  fs.readdir(TEMP_DIR, (err, files) => {
+    if (files.length > 0) {
+      res.redirect(`/api/temp/file/${files[0]}`)
+    } else {
+      res.redirect('/temp');
+    }
+  });
+})
+
+// GET file in temp directory
+server.get('/api/temp/file/:filename', (req, res) => {
+  res.sendFile(TEMP_DIR + req.params.filename, { root: '.' });
+});
+
+// GET list of temp files
+server.get('/api/temp/files', (req, res) => {
+  let output = { route: 'api/temp/file/', delete: 'api/temp/clear/'};
+  fs.readdir(TEMP_DIR, (err, files) => {
+    output.files = files;
+    res.json(output);
   });
 });
+
+// POST temp file for upload
+// removes previous temp file(s)
+server.post('/api/temp/upload', (req, res) => {
+  fs.readdir(TEMP_DIR, (err, files) => {
+    files.forEach((value) => {
+      fs.unlink(TEMP_DIR + value, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  });
+  addFile(req, res, TEMP_DIR, '/temp');
+});
+
+// Add files posted in request to specified directory
+function addFile(req, res, dir, redirect) {
+  redirect = redirect != undefined ? redirect : '/';
+  const form = formidable({ maxFileSize: MAX_FILE_SIZE });
+  form.parse(req, (err, fields, files) => {
+    fs.rename(files.filetoupload.path, dir + files.filetoupload.name, (err) => {
+      if (err) throw err;
+      res.redirect(redirect);
+    });
+  });
+}
 
 // Listen for connections
 server.listen(process.env.PORT, () => {
